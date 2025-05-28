bc3f71ab39bc635cfd2aafdc3a293a9ba71539fe
GustavonBrito <gustavoserafio@gmail.com>
2025-05-28 14:59:56 -0300

    feat: minishell header updated with includes

---
+++ includes/minishell.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/28 00:41:22 by gustavo-lin       #+#    #+#             */
+/*   Updated: 2025/05/28 13:05:09 by gustavo-lin      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdlib.h>
+#include <signal.h>
+
+#endif
