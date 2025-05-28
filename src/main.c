3b9958d97d9a485beef17d6a615556f6e4b863e0
GustavonBrito <gustavoserafio@gmail.com>
2025-05-28 16:06:40 -0300

    feat: main updated to no content, with that we can start to code on other branch and merge here

--- src/main.c
+++ src/main.c
@@ -6,24 +6,13 @@
 /*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 00:45:31 by gustavo-lin       #+#    #+#             */
-/*   Updated: 2025/05/28 12:51:41 by gustavo-lin      ###   ########.fr       */
+/*   Updated: 2025/05/28 16:05:44 by gustavo-lin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <stdlib.h>
+#include "../includes/minishell.h"
 
 int main(void)
 {
-	while (1)
-	{
-		char *line = readline("minishell$ ");
-		if (line && *line)
-			add_history(line);
-		printf("Linha lida: %s\n", line);
-		free(line);
-	}
-	return(0);
+	// Main here
 }
