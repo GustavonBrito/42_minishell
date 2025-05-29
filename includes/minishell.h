799d437cfea9babc0e9608015a54a5db567cb992
GustavonBrito <gustavoserafio@gmail.com>
2025-05-28 23:11:15 -0300

    feat: updated minishell.h to import libft functions

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 00:41:22 by gustavo-lin       #+#    #+#             */
-/*   Updated: 2025/05/28 13:05:09 by gustavo-lin      ###   ########.fr       */
+/*   Updated: 2025/05/28 23:04:25 by gustavo-lin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,11 @@
 #include <readline/history.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <unistd.h>
+#include "../libft/headers/libft.h"
+#include "../libft/headers/get_next_line.h"
+#include "../libft/headers/ft_printf.h"
+
+void	shell_loop(void);
 
 #endif
