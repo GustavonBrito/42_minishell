19e3f9e1a9def6c2160968d39b90b3279fed9b08
Luiza Kormann <109827640+luizakormann@users.noreply.github.com>
2025-06-04 18:24:13 -0300

    fix codeblock end

--- src/shell_loop.c
+++ src/shell_loop.c
@@ -6,7 +6,7 @@
 /*   By: luiza <luiza@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 17:50:24 by gustavo-lin       #+#    #+#             */
-/*   Updated: 2025/06/04 18:13:09 by luiza            ###   ########.fr       */
+/*   Updated: 2025/06/04 18:19:29 by luiza            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ void	shell_loop(void)
 		buffer_received = readline("minishell$ ");
 		check_exit_condition(buffer_received);
 		if (*buffer_received)
+		{
 			add_history(buffer_received);
 			process_input(buffer_received);
 		}
