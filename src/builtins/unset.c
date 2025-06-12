31c5915f789c41cf1d063a7b9e6e1044b81cfc7e
GustavonBrito <gustavoserafio@gmail.com>
2025-06-11 23:41:03 -0300

    feat: unset to delete a env variable

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,13 +6,16 @@
 /*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 13:31:34 by gustavo-lin       #+#    #+#             */
-/*   Updated: 2025/06/06 14:22:51 by gustavo-lin      ###   ########.fr       */
+/*   Updated: 2025/06/11 11:30:47 by gustavo-lin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void unset(void)
+void unset(char *argv)
 {
+	char **buffer;
 	
+	buffer = ft_split(argv, ' ');
+	unsetenv(buffer[1]);
 }
