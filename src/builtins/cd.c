eedcbdb3a46aab2a0301736f29e43848d1c0fcae
GustavonBrito <gustavoserafio@gmail.com>
2025-06-13 16:05:58 -0300

    feat: deleted comments to push to branch

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 21:47:58 by gustavo-lin       #+#    #+#             */
-/*   Updated: 2025/06/11 13:41:27 by gustavo-lin      ###   ########.fr       */
+/*   Updated: 2025/06/13 16:01:11 by gustavo-lin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,6 @@
 
 void cd(char *argv)
 {
-	(void)argv;
-	// chdir("Área de trabalho");
-	// char *cwd = getcwd(NULL, 0);
-	// ft_printf("%s", cwd);
-	// if (chdir("/home/gustavo-linux") == -1)
-    // {
-    //     perror("chdir");
-    //     return ;
-    // }
-
-    // char *cwd = getcwd(NULL, 0);
-    // if (cwd)
-    // {
-    //     ft_printf("Diretório atual: %s\n", cwd);
-    //     free(cwd);
-    // }
 	char 	**buffer;
 	char 	*home;
 	char	*actual_directory;
