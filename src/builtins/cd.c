8010151d76af0dccc778de362d1ef3ba24296fc9
GustavonBrito <gustavoserafio@gmail.com>
2025-06-18 10:57:32 -0300

    feat: has to implement escape space with '\'

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 21:47:58 by gustavo-lin       #+#    #+#             */
-/*   Updated: 2025/06/17 19:31:24 by gustavo-lin      ###   ########.fr       */
+/*   Updated: 2025/06/17 23:05:44 by gustavo-lin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,10 @@
 
 void cd(char *argv, t_token *token_lst)
 {
-	//char 	**buffer;
 	char 	*home;
 	char	*actual_directory;
 	
-	(void) argv;
-	// buffer = ft_split(argv, ' ');
-	// if (buffer[1] == NULL)
-	// {
-	// 	home = malloc(sizeof(char) * ft_strlen(getenv("HOME")) + 1);
-	// 	ft_strlcpy(home, getenv("HOME"), ft_strlen(getenv("HOME") + 1));
-	// 	chdir(home);
-	// 	free(home);
-	// }
-	// if (chdir(buffer[1]) == -1)
-	// {
-	// 	ft_printf("No such file or directory: %s\n", buffer[1]);
-	// 	return ;
-	// }
+	(void)argv;
 	if (token_lst->next != NULL)
 	{
 		if (chdir(token_lst->next->value) == -1)
