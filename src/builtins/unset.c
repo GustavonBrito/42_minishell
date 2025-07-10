6461ae8023f126aad157f007128b4763cefcb0c6
GustavonBrito <gustavoserafio@gmail.com>
2025-07-09 23:18:51 -0300

    feat:doing unset

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,7 +6,7 @@
 /*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 13:31:34 by gustavo-lin       #+#    #+#             */
-/*   Updated: 2025/07/07 23:25:43 by gustavo-lin      ###   ########.fr       */
+/*   Updated: 2025/07/09 23:18:13 by gustavo-lin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,29 +27,38 @@ void	unset(char **argv)
 	//t_env	*tmp;
 	t_env	*env;
 	t_env	*new_env;
+	t_env	*head;
+	t_env	*tmp;	
 	int	i;
+	int skip;
 	//int	flag_var_already_exists;
 
-	i = 1;
 	//flag_var_already_exists = 0;
 	env = get_t_env();
 	//tmp = env;
 	new_env = NULL;
-	new_env = malloc(sizeof(t_env));
+	head = NULL;
+	skip = 0;
+	//head = malloc(sizeof(t_env));
 	while(env)
 	{
-		if (ft_strncmp(env->env_data, argv[i], ft_strlen(argv[i])) == 0 && argv[i])
+		i = 1;
+		while (argv[i])
 		{
-			env = env->next;
+			if (ft_strncmp(env->env_data, argv[i], ft_strlen(argv[i])) == 0 && argv[i])
+			{
+				skip = 1;
+				break;
+			}
 			i++;
-			continue;
 		}
+		new_env = malloc(sizeof(t_env));
 		new_env->env_data = ft_strdup(env->env_data);
-		new_env->next = malloc(sizeof(t_env));
-		if (!new_env->next)
-			free(new_env);
+		new_env->next = NULL;
+		if (!head)
+			head = new_env;
 		env = env->next;
 		new_env = new_env->next;
-		new_env->next = NULL;
 	}
+	env = head;
 }
