79dbc2c32492ca7d180edef1291c64517f04b757
Luiza Kormann <109827640+luizakormann@users.noreply.github.com>
2025-08-04 17:16:39 -0300

    lexer/lex_vars.c norm OK

--- src/tokenization/lexer/lex_vars.c
+++ src/tokenization/lexer/lex_vars.c
@@ -6,16 +6,17 @@
 /*   By: luiza <luiza@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 19:21:55 by luiza             #+#    #+#             */
-/*   Updated: 2025/08/04 16:50:50 by luiza            ###   ########.fr       */
+/*   Updated: 2025/08/04 17:15:56 by luiza            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	process_var_name(char *input, t_token **token_lst, int start);
-int	handle_special_vars(char *input, t_token **token_lst, int i);
-int	is_assignment_with_quotes(char *input, int start);
-int	has_adjacent_quotes(char *input, int start);
+int			process_var_name(char *input, t_token **token_lst, int start);
+int			handle_special_vars(char *input, t_token **token_lst, int i);
+int			is_assignment_with_quotes(char *input, int start);
+int			has_adjacent_quotes(char *input, int start);
+static int	token_length(char *input, int i);
 
 int	process_var_name(char *input, t_token **token_lst, int start)
 {
@@ -64,18 +65,26 @@ int	is_assignment_with_quotes(char *input, int start)
 
 int	has_adjacent_quotes(char *input, int start)
 {
-	int	i;
+	int		i;
 
 	i = start;
 	if (i > 0 && !ft_isspace(input[i - 1])
-		&& !ft_isop(input[i - 1]) && input[i - 1] != '$')
+		&& !ft_isop(input[i - 1]) && input[i - 1] != '\'')
 		return (1);
+	i = token_length(input, i);
+	return (i > start + 2);
+}
+
+static int	token_length(char *input, int i)
+{
+	char	quote_char;
+
 	while (input[i] && !ft_isspace(input[i])
-		&& !ft_isop(input[i]) && input[i] != '$')
+		&& !ft_isop(input[i]) && input[i] != '\'')
 	{
 		if (input[i] == '\'' || input[i] == '"')
 		{
-			char	quote_char = input[i];
+			quote_char = input[i];
 			i++;
 			while (input[i] && input[i] != quote_char)
 				i++;
@@ -85,5 +94,5 @@ int	has_adjacent_quotes(char *input, int start)
 		else
 			i++;
 	}
-	return (i > start + 2);
+	return (i);
 }
