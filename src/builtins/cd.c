7698f172eff8e985c0fe81921f7d03cbf9ba01ab
GustavonBrito <gustavoserafio@gmail.com>
2025-09-01 09:29:54 -0300

    fix: cd behavior fixed, now its assigning a exit value and stoped doing seg fault

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: gustavo <gustavo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 21:47:58 by gustavo-lin       #+#    #+#             */
-/*   Updated: 2025/08/17 16:59:48 by gustavo          ###   ########.fr       */
+/*   Updated: 2025/09/01 09:25:42 by gustavo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ void	cd(t_command *cmd)
 	if (cmd->args[2] != NULL)
 	{
 		write(2, "minishell: cd: too many arguments\n", 34);
-		free_env_list(*handle_t_env(NULL));
 		rl_clear_history();
-		exit(1);
+		g_exit_status = 1;
+		return ;
 	}
 	if (!cmd->args[1])
 	{
@@ -52,9 +52,9 @@ static void	cd_to_target(char *target_dir)
 	if (chdir(target_dir) == -1)
 	{
 		write(2, "minishell: cd: No such file or directory\n", 41);
-		free_env_list(*handle_t_env(NULL));
 		rl_clear_history();
-		exit(1);
+		g_exit_status = 1;
+		return ;
 	}
 	update_pwd();
 }
