e8b4d65f9b8595dbdb03163fba47e9afe9ffd111
Gustavo Serafio Correa De Brito <gserafio@c3r7p2.42sp.org.br>
2025-09-18 12:10:11 -0300

    feat: adding fix on unset and g_exit_status on buffer NULL

--- src/builtins/unset_utils.c
+++ src/builtins/unset_utils.c
@@ -6,7 +6,7 @@
 /*   By: gserafio <gserafio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/18 05:20:01 by gserafio          #+#    #+#             */
-/*   Updated: 2025/09/18 12:07:33 by gserafio         ###   ########.fr       */
+/*   Updated: 2025/09/18 12:10:03 by gserafio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ t_env	*create_filtered_list(t_command *cmd, t_env *s_env)
 	{
 		if (!verify_remove_env(cmd->args, s_env))
 		{
-			tmp = calloc(1, sizeof(t_env));
+			tmp = ft_calloc(1, sizeof(t_env));
 			if (!tmp)
 				break ;
 			tmp->env_data = ft_strdup(s_env->env_data);
