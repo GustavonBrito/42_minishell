f865a175c730173ebe521b309c99055b030713db
Luiza Kormann Carvalho Da Fonseca <lukorman@student.42sp.org.br>
2025-09-18 06:25:26 -0300

    fix: pipe_utils2.c norma ok minor changes

--- src/pipes/pipe_utils2.c
+++ src/pipes/pipe_utils2.c
@@ -6,7 +6,7 @@
 /*   By: lukorman <lukorman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/17 11:44:21 by gustavo           #+#    #+#             */
-/*   Updated: 2025/09/18 05:33:13 by lukorman         ###   ########.fr       */
+/*   Updated: 2025/09/18 06:23:38 by lukorman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int				validate_pre_fork(t_command *cmd_crr, t_command *first_cmd);
 
 void	init_pipe_struct(void)
 {
-	t_pipe_mode *pipe;
+	t_pipe_mode	*pipe;
 
 	pipe = NULL;
 	pipe = ft_calloc(1, sizeof(t_pipe_mode));
@@ -28,16 +28,18 @@ void	init_pipe_struct(void)
 t_pipe_mode	**handle_pipe_mode(void)
 {
 	static t_pipe_mode	*pipe;
+
 	return (&pipe);
 }
-void		free_pipe_mode(t_pipe_mode *head)
+
+void	free_pipe_mode(t_pipe_mode *head)
 {
 	free(head);
 }
 
 int	validate_pre_fork(t_command *cmd_crr, t_command *first_cmd)
 {
-	t_env *env;
+	t_env	*env;
 
 	env = (*handle_t_env(NULL));
 	if (!cmd_crr || !cmd_crr->args || !cmd_crr->args[0])
